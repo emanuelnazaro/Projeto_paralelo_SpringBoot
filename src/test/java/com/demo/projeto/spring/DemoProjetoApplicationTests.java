313d579d2e9f40903b50e887a7f686abe669669f
emanuelnazaro <juniorhealy2010@hotmail.com>
2020-05-23 03:47:00 -0300

    icluindo formulario e links de menu

---
+++ src/test/java/com/demo/projeto/spring/DemoProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.projeto.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
